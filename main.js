1f0b2217f3ebfa6face4b13cc0ed4feef00d2f8f
Vanessa Alves de Souza <92494537+Van-Dev2021@users.noreply.github.com>
2022-01-12 01:02:05 -0300

    Update main.js
    Correção de variáveis.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const LinkSocialMedia = {
+const LinksSocialMedia = {
   github: 'Van-Dev2021',
   youtube: 'van.dev2021',
   facebook: 'van.dev2021',
-  istagram: 'van.dev2021'
+  instagram: 'van.dev2021'
 }
 
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
-    li.children[0].href = `https://${social}.com/${LinkSocialMedia[social]}`
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
 }
 
 changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
-  const url = `https://api.github.com/users/${LinkSocialMedia.github}`
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
   fetch(url)
     .then(response => response.json())
